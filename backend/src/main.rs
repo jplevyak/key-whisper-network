e6ea2eb1ba533ffb35560821020cb61ec155c99c
John Plevyak <jplevyak@gmail.com>
2025-05-01 11:07:16 -0700

    Interim
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -355,8 +355,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let governor_config = Arc::new(
         GovernorConfigBuilder::default()
             .key_extractor(SmartIpKeyExtractor) // Use SmartIpKeyExtractor for X-Real-IP
-            .per_second(10)
-            .burst_size(10)
+            .per_second(100)
+            .burst_size(100)
             .finish()
             .unwrap(),
     );
